c56ccf2bf028c1547adf102649b6a0e98a4b7d3c
unknown <princekwk@gmail.com>
2019-04-23 23:56:45 -0500

    draft 2

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path   = require('path');
 const port   = 5000;
 
 app.use(express.static(path.join(__dirname + '/public')));
-// app.use(express.static(path.join(__dirname + '/public/bower_components')));
-// app.use(express.static(path.join(__dirname + '/public/dist')));
+app.use(express.static(path.join(__dirname + '/public/bower_components')));
+app.use(express.static(path.join(__dirname + '/public/dist')));
 app.set('view engine','ejs')
 
 app.get("/", (req, res) => {
@@ -14,11 +14,11 @@ app.get("/", (req, res) => {
 })
 
 app.get("/home", (req, res) => {
-	 res.sendFile(path.join(__dirname + '/views/Home.html'));
+	 res.sendFile(path.join(__dirname + '/Home.html'));
 })
 
 app.get("/accounting", (req, res) => {
-	 res.sendFile(path.join(__dirname + '/views/home_pages_all.html'));
+	 res.sendFile(path.join(__dirname + '/home_pages_all.html'));
 })
 
 app.listen(process.env.PORT || 5000, () => {
